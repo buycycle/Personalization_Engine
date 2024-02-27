068a8f0c7bda2ee00f0fb7fad38b6ab0b7b600c6
rauner <29773634+rauner@users.noreply.github.com>
2024-02-27 10:36:47 +0100

    finalize version 004

--- model/app.py
+++ model/app.py
@@ -40,7 +40,7 @@ app = FastAPI()
 environment = os.getenv("ENVIRONMENT")
 ab = os.getenv("AB")
 app_name = "recommender-system"
-app_version = "stable-003"
+app_version = "stable-004"
 
 KAFKA_TOPIC = config["KAFKA"]["topic_recommendation"]
 KAFKA_BROKER = config["KAFKA"]["broker"]
