c28f182119195de807d811982bcd4494bf053917
rauner <29773634+rauner@users.noreply.github.com>
2024-03-14 10:46:39 +0100

    add status code and model-version counter for request.bash

--- request.bash
+++ request.bash
@@ -1,5 +1,48 @@
-url="https://dev.recommendation.buycycle.com/recommendation"
-data='{"bike_id": 14394, "user_id": 123, "distinct_id": "8130798c-db73-41f1-8d29-4b14079174a2", "n": 8, "family_id": 12}'
-# Use seq to generate a list of numbers from 1 to 1000, then pipe it to xargs
-seq 1000 | xargs -I % -P 1000 sh -c "curl -i -X POST '$url' -H 'Content-Type: application/json' -d '$data'"
+#!/bin/bash
+url="https://ab.recommendation.buycycle.com/recommendation"
+data_template='{
+  "strategy": "product_page",
+  "bike_id": 21091,
+  "user_id": 18742,
+  "device_id": "bccd547b-6736-41a2-8543-677b724fb2a1",
+  "frame_size_code":"58",
+  "price": 2750,
+  "family_id": 803,
+  "n": 12
+}'
+# Total number of requests to send
+total_requests=100
+# Initialize an associative array to hold the count of HTTP status codes and versions
+declare -A status_code_count
+declare -A version_count
+# Print initial information
+current_date_time=$(date '+%Y-%m-%d %H:%M:%S')
+echo "Date and Time: $current_date_time"
+echo "Address: $url"
+echo "Request Payload: $data_template"
+echo "Total requests to be sent: $total_requests"
+# Loop total_requests times
+for i in $(seq 1 $total_requests); do
+  # Execute the curl command and capture the headers and body
+  response=$(curl -s -i -X POST "$url" -H "Content-Type: application/json" -d "$data_template")
 
+  # Extract the HTTP status code and version from the response
+  http_code=$(echo "$response" | grep HTTP | tail -1 | awk '{print $2}')
+  version=$(echo "$response" | grep -oP 'version: \K.*')
+
+  # Increment the count for this HTTP status code and version
+  ((status_code_count[$http_code]++))
+  ((version_count[$version]++))
+done
+# Print the count of each HTTP status code
+echo "HTTP Status Code Counts:"
+for code in "${!status_code_count[@]}"; do
+  echo "Status Code $code: ${status_code_count[$code]}"
+done
+# Print the count of each version
+echo "Version Counts:"
+for ver in "${!version_count[@]}"; do
+  echo "Version $ver: ${version_count[$ver]}"
+done
+# Print completion information
+echo "Total requests sent: $total_requests"
