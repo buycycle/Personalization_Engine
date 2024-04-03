150225cf7907890d6382adac340c82d6470e0ae8
rauner <29773634+rauner@users.noreply.github.com>
2024-04-03 15:17:52 +0200

    update request.bash

--- request.bash
+++ request.bash
@@ -11,7 +11,7 @@ data_template='{
   "n": 12
 }'
 # Total number of requests to send
-total_requests=10
+total_requests=100
 # Initialize an associative array to hold the count of HTTP status codes and versions
 declare -A status_code_count
 declare -A version_count
