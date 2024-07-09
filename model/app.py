21c79ba08d335b8ead380c4a4db457ece55abbf0
rauner <29773634+rauner@users.noreply.github.com>
2024-07-09 13:46:33 +0200

    name new canary model version

--- model/app.py
+++ model/app.py
@@ -47,7 +47,7 @@ app = FastAPI()
 environment = os.getenv("ENVIRONMENT")
 ab = os.getenv("AB")
 app_name = "recommender-system"
-app_version = "canary-007"
+app_version = "canary-008-continent"
 
 logger = Logger.configure_logger(environment, ab, app_name, app_version)
 logger.info("FastAPI app started")
